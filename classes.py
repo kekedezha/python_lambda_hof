2510c8ab61d8bf77244ae1a5ef2e719d6da8239b
kekedezha <dezhac6@gmail.com>
2024-08-25 22:04:02 -0400

    added comments for examples of oop

--- classes.py
+++ classes.py
@@ -1,3 +1,6 @@
+# Object Oriented Programming
+# Classes - Classes provide a means of bundling data and functionality together. Creating a new class creates a new type of object, allowing new instances of that type to be made. Each class instance can have attributes attached to it for maintaining its state. Class instances can also have methods (defined by its class) for modifying its state.
+
 class Vehicle:
     def __init__(self, make, model):
         self.make = make
@@ -21,6 +24,7 @@ your_car = Vehicle('Cadillac', 'Escalade')
 your_car.get_make_model()
 your_car.moves()
 
+# Inheritance - in OOP, when a class derives from another class. The child class will inherit all the public and protected properties and methods from the parent class. In addition, it can have its own properties and methods.
 
 class Airplane(Vehicle):
     def __init__(self, make, model, faa_id):
@@ -53,6 +57,7 @@ golfwagon.moves()
 
 print('\n\n')
 
+# Polymorphism - The word "polymorphism" means "many forms", and in programming it refers to methods/functions/operators with the same name that can be executed on many objects or classes.
 for v in (my_car, your_car, cessna, mack, golfwagon):
     v.get_make_model()
     v.moves()
