c0fe294fbaa412f079b5f35b452ba807ce78e95d
Christian Dezha-Bolteada <105559393+kekedezha@users.noreply.github.com>
2024-09-05 22:37:59 -0400

    Write to file practice added

--- files.py
+++ files.py
@@ -35,3 +35,12 @@ f.close()
 f = open("names.txt")
 print(f.read())
 f.close()
+
+# Write (overwrite)
+f = open("context.txt", "w")
+f.write("I deleted all of the context")
+f.close()
+
+f = open("context.txt")
+print(f.read())
+f.close()
